2b15aa4a429cb9eaf46e456491e63f03e437ae41
Konrad Polek <polekk@tt.com.pl>
2019-01-03 10:13:22 +0100

    bluetooth detection fixed on API>=23

--- app/src/main/java/pl/com/tt/kapp/MainActivity.kt
+++ app/src/main/java/pl/com/tt/kapp/MainActivity.kt
@@ -1,15 +1,25 @@
 package pl.com.tt.kapp
 
+import android.Manifest
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.main_layout.*
 
 class MainActivity : AppCompatActivity() {
 
+    val MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
         setSupportActionBar(toolbar)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION)
+        }
+
     }
 
 }
