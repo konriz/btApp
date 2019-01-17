eaf949df2d3b923fca8716644acbdaec3f0fffb6
Konrad Polek <polekk@tt.com.pl>
2019-01-17 14:15:40 +0100

    Menu button without actions

--- app/src/main/java/pl/com/tt/kapp/MainActivity.kt
+++ app/src/main/java/pl/com/tt/kapp/MainActivity.kt
@@ -7,7 +7,11 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.*
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
 import android.util.Log
+import android.view.MenuItem
 import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.main_layout.*
 import pl.com.tt.kapp.modules.location.model.FusedLocationProvider
@@ -15,16 +19,43 @@ import pl.com.tt.kapp.modules.location.model.FusedLocationProvider
 
 const val TAG = "Main Activity"
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var mDrawerLayout : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+        }
+        
+        mDrawerLayout = drawer_layout
+        val navigationView = navigation_view
+        navigationView.setNavigationItemSelectedListener { 
+            menuItem ->
+            menuItem.isChecked = true
+            mDrawerLayout.closeDrawers()
+            true
+        }
+        
 
         FusedLocationProvider.gpsService = LocationServices.getFusedLocationProviderClient(this)
-        setSupportActionBar(toolbar)
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             askForPermissions()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
